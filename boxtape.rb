75ad553b472bb325fecc8a214e03ceae11eb9b79
martin.pitt <martin.pitt@lab49.com>
2015-09-08 15:31:50 +0100

    Added version

--- boxtape.rb
+++ boxtape.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class Boxtape < Formula
   homepage "http://boxtape.io"
-  url "http://localhost:8000/boxtape.zip"
+  url "http://localhost:8000/boxtape-0.4.zip"
 
   def install
       inreplace "brew/boxtape", "##PREFIX##", "#{prefix}"
