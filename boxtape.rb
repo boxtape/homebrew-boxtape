3def63a0123f5cb3be6953421019932ac771c25e
martin.pitt <martin.pitt@lab49.com>
2015-09-08 15:08:23 +0100

    Spiking brew scripts

--- boxtape.rb
+++ boxtape.rb
@@ -2,11 +2,11 @@ require "formula"
 
 class Srcclr < Formula
   homepage "http://boxtape.io"
-  url "https://download.sourceclear.com/console/srcclr-console-0.7.0.zip"
+  url "http://localhost:8000/boxtape.zip"
 
   def install
-      inreplace "brew/srcclr", "##PREFIX##", "#{prefix}"
-      prefix.install "srcclr-console.jar"
-      bin.install "brew/srcclr"
+      inreplace "brew/boxtape", "##PREFIX##", "#{prefix}"
+      prefix.install "lib"
+      bin.install "bin/boxtape"
   end
 end
